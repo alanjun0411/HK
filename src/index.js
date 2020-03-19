b9424bd6b074ba28968b6c4c6a5f76bc4af69c13
jack <jackjun8090@163.com>
2020-03-19 19:57:16 +0800

    加入字体图标和配置TabBar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './assct/icofont.css'
 import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
